1199fc9e6f1257b1eccf0ddd5b3e65232149e1ff
Ben Tracy <benatracy@gmail.com>
2018-02-19 23:52:35 +0000

    Adds typedef for absurdly long priority queue in astarpathfinder

--- src/Pathfinder/AStarPathfinder.cpp
+++ src/Pathfinder/AStarPathfinder.cpp
@@ -4,12 +4,14 @@
 #include <queue>
 #include <functional>
 #include <glm/glm.hpp>
+
+typedef std::priority_queue<std::pair<int, std::shared_ptr<GraphNode>>, std::vector<std::pair<int, std::shared_ptr<GraphNode>>>, std::greater<std::pair<int, std::shared_ptr<GraphNode>>>> NodeQueue;
 PathResult AStarPathfinder::FindPath(const Graph& graph, int start_index, int end_index) const
 {
 	PathResult result;
 	std::unordered_map<std::shared_ptr<GraphNode>, int> cost_so_far;
 	std::unordered_map<std::shared_ptr<GraphNode>, std::shared_ptr<GraphNode>> came_from;
-	std::priority_queue<std::pair<int, std::shared_ptr<GraphNode>>, std::vector<std::pair<int, std::shared_ptr<GraphNode>>>, std::greater<std::pair<int, std::shared_ptr<GraphNode>>>> nodes;
+	NodeQueue nodes;
 	auto start_node = graph.GetNode(start_index).lock();
 	auto end_node = graph.GetNode(end_index).lock();
 	nodes.emplace(0, start_node);
