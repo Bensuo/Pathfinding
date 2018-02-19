7bba4da16cfe37e19a542327760012567eca093a
Ben Tracy <benatracy@gmail.com>
2018-02-19 15:54:51 +0000

    Updated build script and cmakelists to produce executable

--- BUILD-WINDOWS-VS2017-X64.bat
+++ BUILD-WINDOWS-VS2017-X64.bat
@@ -3,4 +3,4 @@ title Pathfinding source file generation
 echo Starting generation of project source files using cmake
 if not exist "\build\" mkdir build
 cd build
-cmake cmake -G "Visual Studio 15 2017 Win64" src/
+cmake cmake -G "Visual Studio 15 2017 Win64" ../src/
