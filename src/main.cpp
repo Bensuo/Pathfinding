56543b64096c57756b6252a00da9ecc6b6c90fef
Stuart Adams <stuartdadams@live.com>
2018-02-20 00:09:12 +0000

    Updated program output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Graph\DotParser.hpp"
 #include "Pathfinder/DijkstraPathfinder.hpp"
 #include <iostream>
+#include "Pathfinder/AStarPathfinder.hpp"
 
 void PrintPath(const IPathfinder& pathfinder, const Graph& graph, int start_node, int end_node)
 {
@@ -19,8 +20,10 @@ int main(int argc, char* argv[])
 	DotParser parser;
 	const auto graph = parser.Read("graph.dot");
 
+    std::cout << "A* Pathfinder" << std::endl;
     PrintPath(AStarPathfinder{}, graph, 0, 63);
-    //PrintPath(DijkstraPathfinder{}, graph, 0, 10);
+    std::cout << "Dijkstra Pathfinder" << std::endl;
+    PrintPath(DijkstraPathfinder{}, graph, 0, 63);
 
     std::cin.get();
 }
